2c119505c2a2e0daf719c3859d782af78018148c
Evan Kasky <ekasky25@gmail.com>
2024-07-24 14:23:53 -0400

    Adds react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-/* Pages */
 import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import ForgotPasswordPage from './pages/ForgotPasswordPage';
-import ResetPasswordPage from './pages/ResetPasswordPage';
-import HomePage from './pages/HomePage';
 
 export default function App() {
 
   return (
+
+    <BrowserRouter>
     
-    <AuthProvider>
-      <BrowserRouter>
+      <Routes>
 
-        <Routes>
+        <Route path='/' element={<LoginPage />} />
 
+      </Routes>
 
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/forgot-password' element={<ForgotPasswordPage />}/>
-          <Route path='/reset-password' element={<ResetPasswordPage />}/>
-
-          {/* Protected Routes */}
-          <Route path='/home' element={<HomePage />} />
-
-          <Route path='*' element={<>Not Found</>} />
-          
-        </Routes>
-
-      </BrowserRouter>
-    </AuthProvider>
+    </BrowserRouter>
 
   );
 
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,10 @@
-import LoginForm from "../components/LoginForm";
 
 
 export default function LoginPage() {
 
-    return (
+    return(
 
-        <LoginForm />
+        <h1>Login Page</h1>
 
     );
 
